7a65b7dda5098e60928b7d013b28d15998328cc0
kvlksgjp1 <41226017+kvlksgjp1@users.noreply.github.com>
2019-05-27 12:26:41 +0900

    "0527 Basic Logic Update"

---
+++ Snake.js
@@ -0,0 +1,50 @@
+var scl = 20;
+var game_speed = 10;
+
+function Snake(){
+	this.x = 0;
+	this.y = 0;
+	this.xspeed = 1;
+	this.yspeed = 0;
+  this.total = 1;
+  this.tail = [];
+
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if(d < 10){
+        this.total++;
+        return true;
+      }else{
+      return false;
+    }
+  }
+
+  this.dir = function(x, y){
+      this.xspeed = x;
+      this.yspeed = y;
+  }
+
+	this.update = function(){
+      if (this.total === this.tail.length){
+        for (var i=0; i<this.tail.length-1; i++){
+          this.tail[i] = this.tail[i+1];
+        }
+      }
+      this.tail[this.total-1] = createVector(this.x, this.y);
+
+		this.x = this.x + this.xspeed * game_speed;
+		this.y = this.y + this.yspeed * game_speed;
+
+    this.x = constrain(this.x, 0, width-scl);
+    this.y = constrain(this.y, 0, height-scl);
+
+	}
+
+	this.show = function(){
+		fill(255);
+    for (var i=0; i<this.tail.length; i++){
+  		rect(this.tail[i].x, this.tail[i].y, 20, 20);
+    }
+    //rect(this.x, this.y, 20, 20);
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var s;
+var food;
+var scl = 20
+
+function setup(){
+	createCanvas(600, 600);
+	s = new Snake();
+	frameRate(10);
+	pickLotation();
+}
+
+function pickLotation(){
+	var cols = floor(width/scl);
+	var rows = floor(height/scl);
+	food = createVector(floor(random(cols)), floor(random(rows)));
+	food.mult(scl);
+}
+
+function draw(){
+	background(51);
+	s.update();
+	s.show();
+	if(s.eat(food)){
+		pickLotation();
+	}
+	fill(255, 0, 100);
+	rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW){
+		s.dir(0, -1);
+	}else if(keyCode === DOWN_ARROW){
+		s.dir(0, 1);
+	}else if(keyCode === RIGHT_ARROW){
+		s.dir(1, 0);
+	}else if(keyCode === LEFT_ARROW){
+		s.dir(-1, 0);
+	}
+}
